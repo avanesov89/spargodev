5bbe3d3d8aed2f56dfeffb6a1d94431a069c8370
yu.avanesov <avanesov89@mail.ru>
2021-11-18 12:15:46 +0300

    add new files

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,18 @@
   * @param {number} first - первое число
   * @returns {number}
   */
+
+
+ const details = Array.from(document.querySelectorAll("details"));
+
+ // Add the onclick listeners.
+ details.forEach((targetDetail) => {
+   targetDetail.addEventListener("click", () => {
+     // Close all the details that are not targetDetail.
+     details.forEach((detail) => {
+       if (detail !== targetDetail) {
+         detail.removeAttribute("open");
+       }
+     });
+   });
+ });
