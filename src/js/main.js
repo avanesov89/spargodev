161ed8a0b5d4db7811d574144144b66de91aa04b
yu.avanesov <avanesov89@mail.ru>
2021-11-23 17:40:59 +0300

    edit files

--- src/js/main.js
+++ src/js/main.js
@@ -19,3 +19,59 @@
      });
    });
  });
+
+
+    
+  // MENU OPEN
+  
+//Открыть меню по клику на триггер
+$('.menu-trigger').on('click', function(event){
+  event.preventDefault();
+  toggleMenu(true);
+});
+//Закрыть меню
+$('.close-trigger, .body-overlay').on('click', function(event){
+  event.preventDefault();
+  toggleMenu(false);
+});
+//Добавить или убрать класс
+function toggleMenu(bool) {
+      $('body, .body-overlay, .a-menu').toggleClass('menu-open', bool);
+  };
+  
+  // SUBMENU
+  
+$('.menu__item-drop').on('click', function(){
+  if( $('.menu__item-drop').hasClass('open') ) {
+              $('.menu__item-drop, .sub-menu').removeClass('open');
+          } else {
+              $('.menu__item-drop, .sub-menu').addClass('open');
+          };
+  });
+
+
+  $('.show-right').on('click', function(event){
+  $('.side-left').addClass('shifted');
+      $('.side-right').removeClass('shifted');
+});
+
+$('.show-left').on('click', function(event){
+  $('.side-right').addClass('shifted');
+      $('.side-left').removeClass('shifted');
+});
+
+
+
+
+  $(".header__search").click(function() {
+      $(".search__fixed").css({"display":"block"});
+  });
+
+
+  $(document).mouseup(function (e){ 
+		var div = $(".search__fixed");
+		if (!div.is(e.target) 
+		    && div.has(e.target).length === 0) { 
+			div.hide(); 
+		}
+	});
