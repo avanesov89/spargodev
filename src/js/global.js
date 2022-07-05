76a25610489697e781b58c894e897d7c3c305dd9
yu.avanesov <avanesov89@mail.ru>
2022-07-05 16:58:45 +0300

    edit files

--- src/js/global.js
+++ src/js/global.js
@@ -1,3 +1,48 @@
-console.log('global');
+/* global jQuery */
++(function ($) {
+  $.init = {
+    doc: $(document),
+
+    // Скрипт-ловушка слайдеров
+    slick: function ($container) {
+      var $selector = ('undefined' != typeof $container) ? $container.find('[data-slick]') : $('[data-slick]');
+
+      if (0 < $selector.length) {
+        $.each($selector, function () {
+          var $this = $(this);
+          var options = $this.data('slick-options');
+
+          if ('undefined' != typeof options) {
+            $this.slick(options);
+          }
+        });
+      }
+    },
+
+    vacancy_form_load: function() {
+      this.doc.on('change', '.wpcf7-form input[type="file"]', function (event) {
+        //event.preventDefault();
+        const $input = $(event.currentTarget);
+        const filename = $input.val().replace(/C:\\fakepath\\/i, '');
+        const inputName = $input.attr('name');
+        const $label = $input.closest('form').find('label[for="'+inputName+'"]');
+        if (filename.length > 0) {
+          $label.find('.field-file__label--text').text(filename);
+        }
+      });
 
 
+    }
+  }
+}(jQuery));
+
+/* Запуск скриптов для фронтенда */
++(function ($) {
+  'use strict';
+
+  // Инициализация сторонних скрипотв
+  $.init.slick();
+
+  $.init.vacancy_form_load();
+
+}(jQuery));
--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,44 @@
 /**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
+ * название функции
+ *
+ * @param {number} first - первое число
+ * @returns {number}
+ */
 
 
 
 $(function() {
   var btn = $(".header__search"), block = $(".search__fixed");
   $(document).click(function(event) {
-          var target = $(event.target),cur;
-          if (target.is(".header__search")) {
-                  event.preventDefault();
-                  var index = btn.index(target);
-                  cur = block.eq(index).fadeToggle();
-                  target.toggleClass("open")
-            }
-          if (target.closest(".search__fixed").length) return;
-          block.not(cur).fadeOut("slow");
-          btn.not(target).removeClass("open")
+    var target = $(event.target),cur;
+    if (target.is(".header__search")) {
+      event.preventDefault();
+      var index = btn.index(target);
+      cur = block.eq(index).fadeToggle();
+      target.toggleClass("open")
+    }
+    if (target.closest(".search__fixed").length) return;
+    block.not(cur).fadeOut("slow");
+    btn.not(target).removeClass("open")
   })
 });
 
 // Accordeon
- const details = Array.from(document.querySelectorAll("details"));
- // Add the onclick listeners.
- details.forEach((targetDetail) => {
-   targetDetail.addEventListener("click", () => {
-     // Close all the details that are not targetDetail.
-     details.forEach((detail) => {
-       if (detail !== targetDetail) {
-         detail.removeAttribute("open");
-       }
-     });
-   });
- });
+const details = Array.from(document.querySelectorAll("details"));
+// Add the onclick listeners.
+details.forEach((targetDetail) => {
+  targetDetail.addEventListener("click", () => {
+    // Close all the details that are not targetDetail.
+    details.forEach((detail) => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
 
 
-    
+
 // MENU OPEN
 //Открыть меню по клику на триггер
 $('.menu-trigger').on('click', function(event){
@@ -52,27 +52,31 @@ $('.close-trigger, .body-overlay').on('click', function(event){
 });
 //Добавить или убрать класс
 function toggleMenu(bool) {
-      $('body, .body-overlay, .a-menu').toggleClass('menu-open', bool);
-  };
-  
+  $('body, .body-overlay, .a-menu').toggleClass('menu-open', bool);
+};
+
 // SUBMENU
 $('.menu__item-drop').on('click', function(){
   if( $('.menu__item-drop').hasClass('open') ) {
-              $('.menu__item-drop, .sub-menu').removeClass('open');
-          } else {
-              $('.menu__item-drop, .sub-menu').addClass('open');
-          };
-  });
-
-
-
-
-$('.clients__list').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4
+    $('.menu__item-drop, .sub-menu').removeClass('open');
+  } else {
+    $('.menu__item-drop, .sub-menu').addClass('open');
+  };
 });
 
 
+
+ 
+ $(function() {
+  $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+        $('#toTop').fadeIn();
+          } else {
+        $('#toTop').fadeOut();
+      }
+    });
+      $('#toTop').click(function() {
+      $('body,html').animate({scrollTop:0},800);
+  });
+ });
+  
